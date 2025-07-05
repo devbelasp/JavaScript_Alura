9fd67b0c2ca6bd5458c6a06e4bcee2a80674dbd6
devbelasp <isa.bela360@hotmail.com>
2025-07-05 20:24:10 -0300

    Exercícios finalizados.

---
+++ Lógica de programação explore funções e listas/js-curso-2-desafio_4/app.js
@@ -0,0 +1,22 @@
+// 1.Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+
+// 2.Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos:
+// 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+// 3.Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+console.log(linguagensDeProgramacao);
+
+// 4.Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let nomes = ['Isabela', 'Arthur', 'Larissa'];
+console.log(nomes[0]);
+
+// 5.Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+let cores = ['Azul', 'Roxo', 'Preto'];
+console.log(cores[1]);
+
+// 6.Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+let comidasJaponesa = ['Temaki', 'Ramen', 'Udon'];
+console.log(comidasJaponesa[2]);
